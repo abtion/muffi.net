d774872068c021b14218e20c0dfb685f7f286350
MikkelHansenAbtion <meh@abtion.com>
2021-08-12 15:18:22 +0200

    ExampleEntity.cs add Phone, email, annotations
    Co-authored-by: mer-abtion <mer-abtion@users.noreply.github.com>

--- src/MuffiNet.Backend/Models/ExampleEntity.cs
+++ src/MuffiNet.Backend/Models/ExampleEntity.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MuffiNet.Backend.Models
 {
     public class ExampleEntity
     {
+        [Required]
         public int Id { get; set; }
 
-        public string Name {  get; set; }
+        public string Name { get; set; }
 
-        public string Description {  get; set; }
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Phone { get; set; }
     }
 }
