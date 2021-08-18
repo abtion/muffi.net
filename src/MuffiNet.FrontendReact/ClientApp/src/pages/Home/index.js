f9f2e527c261686191e3a0da394a5c9f11e3e547
Mia <mer@abtion.com>
2021-08-18 13:56:58 +0200

    Home page with form and table

--- src/MuffiNet.FrontendReact/ClientApp/src/pages/Home/index.js
+++ src/MuffiNet.FrontendReact/ClientApp/src/pages/Home/index.js
@@ -1,24 +1,33 @@
 import React, { useCallback } from "react"
 import axios from "axios"
-
+import useMappedRecords from "~/hooks/useMappedRecords"
 import Layout from "~/components/Layout"
 import ExampleForm from "~/components/ExampleForm"
+import ExampleTable from "~/components/ExampleTable"
 import useHub from "~/hooks/useHub"
 
 export default function Home() {
+
+  const [exampleEntityMap, upsertExampleEntity, deleteExampleEntity] =
+    useMappedRecords((exampleEntity) => exampleEntity.id)
+   
   const onHubConnected = useCallback((connection) => {
-    connection.on("SomeEntityCreated", ({ message }) => {
-      console.log("SomeEntityCreated", message)
+    connection.on("SomeEntityCreated", ({ exampleEntity }) => {
+      upsertExampleEntity(exampleEntity)
+      console.log("SomeEntityCreated", exampleEntity) 
     })
 
-    connection.on("SomeEntityUpdated", ({ message }) => {
-      console.log("SomeEntityUpdated", message)
+    connection.on("SomeEntityUpdated", ({ exampleEntity }) => {
+      upsertExampleEntity(exampleEntity)
+      console.log("SomeEntityUpdated", exampleEntity)
     })
 
-    connection.on("SomeEntityDeleted", ({ message }) => {
-      console.log("SomeEntityDeleted", message)
+    connection.on("SomeEntityDeleted", ({ exampleEntity }) => {
+      deleteExampleEntity(exampleEntity)
+      console.log("SomeEntityDeleted", exampleEntity)
     })
-  }, [])
+  }, [upsertExampleEntity, deleteExampleEntity])
+
   useHub("/hubs/example", onHubConnected)
 
   const handleSubmit = (formData) => {
@@ -39,6 +48,8 @@ export default function Home() {
   return (
     <Layout>
       <h1 className="text-2xl mb-3">Title</h1>
+      
+      <ExampleTable entities={Object.values(exampleEntityMap)}/>
 
       <ExampleForm onSubmit={handleSubmit} />
     </Layout>
