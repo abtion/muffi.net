19ce9749c3a474a58bb3f079d6b1ce44255a5673
MikkelHansenAbtion <meh@abtion.com>
2021-09-22 11:30:35 +0200

    Refactor frontend: remove exampleContainer

--- src/MuffiNet.FrontendReact/ClientApp/src/pages/AuthorizedHome/test.js
+++ src/MuffiNet.FrontendReact/ClientApp/src/pages/AuthorizedHome/test.js
@@ -1,16 +1,47 @@
 import React from "react"
-import { render as tlRender } from "@testing-library/react"
+import { act, render as tlRender, waitFor } from "@testing-library/react"
+import axios from "axios"
+import userEvent from "@testing-library/user-event"
 import { createMemoryHistory } from "history"
 import { Router } from "react-router"
 
 import AuthorizedHome from "./"
+import useHub from "~/hooks/useHub"
 
-jest.mock("~/components/ExampleContainer", () => (props) => (
-  <mock-ExampleContainer {...props}>
-    <p>ExampleContainer</p>
-    <p>{props.accessToken}</p>
-  </mock-ExampleContainer>
-))
+jest.mock("axios")
+jest.mock("~/hooks/useHub")
+
+const entity = {
+  id: "1",
+  name: "SomeRandomName",
+  description: "someRandomDescription",
+  email: "an@email.dk",
+  phone: "12345678",
+}
+
+const updatedEntity = {
+  ...entity,
+  name: "updated name",
+}
+
+const unnamedEntity = {
+  id: "2",
+  name: undefined,
+  description: "something",
+  email: "some@email.dk",
+  phone: "9999999",
+}
+
+afterEach(() => {
+  axios._reset()
+  useHub._reset()
+})
+beforeEach(() => {
+  axios.get.mockResolvedValue({
+    data: { exampleEntities: [entity, unnamedEntity] },
+  })
+  axios.post.mockResolvedValue({})
+})
 
 function render() {
   const history = createMemoryHistory({
@@ -27,12 +58,132 @@ function render() {
 }
 
 describe(AuthorizedHome, () => {
-  it("renders ExampleContainer", async () => {
-    const { findByText } = render()
-    expect(await findByText("ExampleContainer")).toBeInTheDocument()
+  it("renders two tables", async () => {
+    const { findAllByRole } = render()
+    expect(await findAllByRole("table")).toHaveLength(2)
   })
-  it("serves accessToken to ExampleContainer", async () => {
-    const { findByText } = render()
-    expect(await findByText("accessTokenText")).toBeInTheDocument()
+
+  describe("when submitting the form ", () => {
+    it("posts an unnamed exampleEntity to authorized endpoint", async () => {
+      axios.put.mockResolvedValue({ data: { Id: "1234" } })
+      const { getByLabelText, getByText } = render()
+      userEvent.type(getByLabelText("Description"), "Description")
+      userEvent.type(getByLabelText("E-mail"), "Em@a.il")
+      userEvent.type(getByLabelText("Phone"), "12345678")
+      userEvent.click(getByText("Submit"))
+      await waitFor(() =>
+        expect(axios.put).toHaveBeenCalledWith(
+          "/api/authorizedexample",
+          {
+            Name: "",
+            Description: "Description",
+            Email: "Em@a.il",
+            Phone: "12345678",
+          },
+          { headers: { authorization: "Bearer accessTokenText" } }
+        )
+      )
+    })
+  })
+
+  describe("when clicking remove ", () => {
+    it("calls backend to remove", async () => {
+      const { getAllByRole } = render()
+
+      await waitFor(() =>
+        expect(axios.get).toHaveBeenCalledWith("/api/authorizedexample/all", {
+          headers: { authorization: "Bearer accessTokenText" },
+        })
+      )
+
+      const removeBtn = await getAllByRole("button", {
+        name: /Remove/i,
+      })[0]
+      userEvent.click(removeBtn)
+      await waitFor(() =>
+        expect(axios.post).toHaveBeenCalledWith(
+          "/api/authorizedexample",
+          { id: "1" },
+          {
+            headers: { authorization: "Bearer accessTokenText" },
+          }
+        )
+      )
+    })
+  })
+
+  describe("when entering page", () => {
+    it("gets all current entities", async () => {
+      const { findByText } = render()
+
+      await waitFor(() =>
+        expect(axios.get).toHaveBeenCalledWith("/api/authorizedexample/all", {
+          headers: { authorization: "Bearer accessTokenText" },
+        })
+      )
+
+      expect(await findByText(entity.name)).toBeInTheDocument()
+      expect(await findByText(entity.description)).toBeInTheDocument()
+      expect(await findByText(entity.email)).toBeInTheDocument()
+      expect(await findByText(entity.phone)).toBeInTheDocument()
+
+      expect(await findByText(unnamedEntity.description)).toBeInTheDocument()
+    })
+  })
+
+  describe("hub connection", () => {
+    it("adds new records", async () => {
+      const { findByText } = render()
+
+      act(() => {
+        useHub.connectionMock._trigger("SomeEntityCreated", {
+          entity,
+        })
+      })
+
+      expect(await findByText(entity.name)).toBeInTheDocument()
+      expect(await findByText(entity.description)).toBeInTheDocument()
+      expect(await findByText(entity.email)).toBeInTheDocument()
+      expect(await findByText(entity.phone)).toBeInTheDocument()
+    })
+
+    it("adds & then updates existing records", async () => {
+      const { findByText, queryByText } = render()
+      act(() => {
+        useHub.connectionMock._trigger("SomeEntityCreated", {
+          entity,
+        })
+      })
+
+      act(() => {
+        useHub.connectionMock._trigger("SomeEntityUpdated", {
+          entity: updatedEntity,
+        })
+      })
+
+      expect(queryByText(entity.name)).not.toBeInTheDocument()
+      expect(await findByText(updatedEntity.name)).toBeInTheDocument()
+    })
+
+    it("adds & then deletes records", async () => {
+      const { findByText } = render()
+
+      act(() => {
+        useHub.connectionMock._trigger("SomeEntityCreated", {
+          entity,
+        })
+      })
+
+      const nameElement = await findByText(entity.name)
+      expect(nameElement).toBeInTheDocument()
+
+      act(() => {
+        useHub.connectionMock._trigger("SomeEntityDeleted", {
+          entityId: entity.id,
+        })
+      })
+
+      expect(nameElement).not.toBeInTheDocument()
+    })
   })
 })
--- src/MuffiNet.FrontendReact/ClientApp/src/pages/Home/index.js
+++ src/MuffiNet.FrontendReact/ClientApp/src/pages/Home/index.js
@@ -1,13 +1,81 @@
-import React from "react"
+import React, { useMemo, useCallback, useEffect } from "react"
+import axios from "axios"
+
 import Layout from "~/components/Layout"
-import ExampleContainer from "~/components/ExampleContainer"
+
+import useMappedRecords from "~/hooks/useMappedRecords"
+import ExampleForm from "~/components/ExampleForm"
+import ExampleTable from "~/components/ExampleTable"
+import useHub from "~/hooks/useHub"
 
 export default function Home() {
+  const [exampleEntityMap, upsertExampleEntity, deleteExampleEntity] =
+    useMappedRecords((exampleEntity) => exampleEntity.id)
+
+  const exampleEntityTables = useMemo(() => {
+    const result = {
+      named: [],
+      unnamed: [],
+    }
+    Object.values(exampleEntityMap).forEach((exampleEntity) => {
+      const table = exampleEntity.name ? result.named : result.unnamed
+      table.push(exampleEntity)
+    })
+    return result
+  }, [exampleEntityMap])
+
+  useEffect(() => {
+    axios.get("/api/example/all").then((response) => {
+      const { exampleEntities } = response.data
+      upsertExampleEntity(exampleEntities)
+    })
+  }, [upsertExampleEntity])
+
+  const onHubConnected = useCallback(
+    (connection) => {
+      connection.on("SomeEntityCreated", (message) => {
+        upsertExampleEntity(message.entity)
+      })
+      connection.on("SomeEntityUpdated", (message) => {
+        upsertExampleEntity(message.entity)
+      })
+      connection.on("SomeEntityDeleted", (message) => {
+        deleteExampleEntity({ id: message.entityId })
+      })
+    },
+    [upsertExampleEntity, deleteExampleEntity]
+  )
+  useHub("/hubs/example", onHubConnected)
+
+  const createExampleEntity = (formData) => {
+    return axios.put("/api/example", formData).then((response) => {
+      // console.log(response)
+    })
+  }
+
+  const removeExampleEntity = (id) => {
+    return axios.post("/api/example", { id: id }).then((response) => {
+      // console.log(response)
+    })
+  }
+
   return (
     <Layout>
       <h1 className="text-2xl mb-3">Title</h1>
 
-      <ExampleContainer connectionOptions={undefined} accessToken={undefined} />
+      <ExampleForm onSubmit={createExampleEntity} />
+
+      <h2 className="text-xl mb-2">Named</h2>
+      <ExampleTable
+        entities={exampleEntityTables.named}
+        onRemove={removeExampleEntity}
+      />
+
+      <h2 className="text-xl mb-2 mt-8">Unnamed</h2>
+      <ExampleTable
+        entities={exampleEntityTables.unnamed}
+        onRemove={removeExampleEntity}
+      />
     </Layout>
   )
 }
