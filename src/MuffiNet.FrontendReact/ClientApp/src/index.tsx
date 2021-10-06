8367e1784982843ce3ff987a7a94f968922eafdd
Jesper Sørensen <jeppester1@gmail.com>
2021-10-06 13:52:26 +0200

    Set color variables

--- src/MuffiNet.FrontendReact/ClientApp/src/index.tsx
+++ src/MuffiNet.FrontendReact/ClientApp/src/index.tsx
@@ -4,8 +4,17 @@ import { BrowserRouter } from "react-router-dom"
 import "~/main.scss"
 import App from "~/App"
 
+import prepareColorVariables from "~/utils/prepareColorVariables"
+import colors from "../../../../colors.json"
+
 const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href")
 const rootElement = document.getElementById("root")
+
+const cssRoot: HTMLElement = document.querySelector(":root")
+const cssVariables = prepareColorVariables(colors).cssVariables
+Object.entries(cssVariables).forEach(([name, value]) =>
+  cssRoot.style.setProperty(name, value)
+)
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
