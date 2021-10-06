36807f41ee6149994f5e0847ef286a60bfc5130f
Jesper Sørensen <jeppester1@gmail.com>
2021-10-06 14:02:26 +0200

    Convert App component to typescript

---
+++ src/MuffiNet.FrontendReact/ClientApp/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { Switch, Route } from "react-router"
+
+import AuthorizeRoute from "~/authorization/AuthorizeRoute"
+import ApiAuthorizationRoutes from "~/authorization/ApiAuthorizationRoutes"
+import { ApplicationPaths } from "~/authorization/ApiAuthorizationConstants"
+
+// Visitors
+import Home from "~/pages/Home"
+
+// Authorized pages
+import AuthorizedHome from "~/pages/AuthorizedHome"
+
+export default function App(): JSX.Element {
+  return (
+    <Switch>
+      <Route
+        path={ApplicationPaths.ApiAuthorizationPrefix}
+        component={ApiAuthorizationRoutes}
+      />
+
+      <AuthorizeRoute exact path="/authhome" component={AuthorizedHome} />
+
+      <Switch>
+        <Route exact path={"/"} component={Home} />
+      </Switch>
+    </Switch>
+  )
+}
