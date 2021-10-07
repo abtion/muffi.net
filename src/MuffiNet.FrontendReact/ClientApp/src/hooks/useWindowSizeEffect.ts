ab5f82fd4e736e1526a30bb89f099e5f74069ac0
Jesper Sørensen <jeppester1@gmail.com>
2021-10-07 14:01:56 +0200

    Convert useWindowSizeEffect to use typescript

--- src/MuffiNet.FrontendReact/ClientApp/src/hooks/useWindowSizeEffect.ts
+++ src/MuffiNet.FrontendReact/ClientApp/src/hooks/useWindowSizeEffect.ts
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 
-export default function useWindowSizeEffect(callback) {
+export default function useWindowSizeEffect(callback: () => void) {
   useEffect(() => {
     callback()
     window.addEventListener("resize", callback)
