ff1057d656403569f89c6c10e790293b39166913
Benjamin Rasmussen <br@abtion.com>
2022-05-19 11:11:33 +0200

    EnableRetryOnFailure default values

--- src/MuffiNet.FrontendReact/Startup.cs
+++ src/MuffiNet.FrontendReact/Startup.cs
@@ -21,15 +21,15 @@ public class Startup
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(
-                Configuration.GetConnectionString("DefaultConnection"),
-                sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(2, TimeSpan.FromSeconds(5), null)
-            )
-        );
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection"),
+                    sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()
+                )
+            );
 
         services.AddDatabaseDeveloperPageExceptionFilter();
 
