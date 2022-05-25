9619be29a60a356f05cd5a50c62f7dee5ec39119
Klaus Even Enevoldsen (abtion) <ke@abtion.com>
2022-05-25 10:04:26 +0200

    Added OidcController and removed OidcConfigurationController

---
+++ src/MuffiNet.FrontendReact/Controllers/OidcController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MuffiNet.FrontendReact.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OidcController : ControllerBase
+{
+    [HttpGet("frontend-configuration")]
+    public ActionResult<OpenIdConnectFrontend> FrontendConfiguration([FromServices] IConfiguration configuration, CancellationToken cancellationToken)
+    {
+        return new OpenIdConnectFrontend()
+        {
+            Authority = configuration.GetValue<string>("Authentication:Authority"),
+            ClientId = configuration.GetValue<string>("Authentication:ClientId"),
+            Scopes = configuration.GetValue<string>("Authentication:FrontendScopes"),
+        };
+    }
+
+    public record OpenIdConnectFrontend
+    {
+        public string Authority { get; set; } = default!;
+
+        public string ClientId { get; set; } = default!;
+
+        public string Scopes { get; set; } = default!;
+    }
+}
