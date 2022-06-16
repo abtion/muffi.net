8f18d6493ee4545814ac10401befad1a7010b09f
Klaus Even Enevoldsen (abtion) <ke@abtion.com>
2022-06-16 09:46:43 +0200

    Added Config for connection string

---
+++ src/MuffiNet.FrontendReact/Controllers/ConfigController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MuffiNet.FrontendReact.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        [HttpGet("application-insights-connection-string")]
+        public IActionResult ApplicationInsightsConnectionString([FromServices] IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("ApplicationInsights:ConnectionString");
+
+            return Ok(connectionString);
+        }
+    }
+}
