eae7ea7c36204cd57249be537f3aff4aadf91dd5
Rasmus Schultz <rs@abtion.com>
2022-07-07 09:30:16 +0200

    note

--- src/MuffiNet.FrontendReact/ClientApp/src/index.tsx
+++ src/MuffiNet.FrontendReact/ClientApp/src/index.tsx
@@ -1,4 +1,4 @@
-// TODO upgrade react, react-dom, @types/react, @types/react-dom to version 18 (blocked by oidc-react at this time)
+// TODO upgrade react, react-dom, @types/react, @types/react-dom to version 18 (requires upgrade of @testing-library/react - blocked by oidc-react at this time)
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
