761c424b0d23191f33056dfd24d5d78db9919a78
Klaus Even Enevoldsen (abtion) <ke@abtion.com>
2023-03-06 13:54:52 +0100

    Rename project in entire solution

---
+++ src/Api.Shared/Authentication/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Api.Shared.Authentication.OpenIdConnect;
+
+public static class ServiceCollectionExtensions {
+    public static IServiceCollection AddOidcAuthentication(this IServiceCollection services, ConfigurationManager configuration) {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => {
+                options.Authority = configuration.GetValue<string>("Authentication:Authority");
+                options.ClaimsIssuer = configuration.GetValue<string>("Authentication:ClaimsIssuer");
+                options.TokenValidationParameters = new TokenValidationParameters() {
+                    ValidateIssuer = configuration.GetValue<bool>("Authentication:TokenValidationParameters:ValidateIssuer"),
+                    ValidateAudience = configuration.GetValue<bool>("Authentication:TokenValidationParameters:ValidateAudience"),
+                    ValidateLifetime = configuration.GetValue<bool>("Authentication:TokenValidationParameters:ValidateLifetime"),
+                    ValidateTokenReplay = configuration.GetValue<bool>("Authentication:TokenValidationParameters:ValidateTokenReplay"),
+                    ValidIssuer = configuration.GetValue<string>("Authentication:TokenValidationParameters:ValidIssuer"),
+                    ValidAudience = configuration.GetValue<string>("Authentication:TokenValidationParameters:ValidAudience"),
+                };
+            });
+
+        return services;
+    }
+}
