605c2f7b287f0e6bf0e33b647bfdc0e695c98dd7
Klaus Even Enevoldsen (abtion) <ke@abtion.com>
2024-01-16 10:54:05 +0100

    Rebase merge errors

---
+++ src/Presentation.UserManagement/Queries/LoadUsersAndRolesQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Shared;
+using Domain.UserAdministration.Repositories;
+using Presentation.UserAdministration.Dtos;
+
+namespace Presentation.UserAdministration.Queries;
+
+public class LoadUsersAndRolesQueryHandler(IGetAppRoles GetAppRoles, IGetAppRoleAssigment GetAppRoleAssigment) : IQueryHandler<LoadUsersAndRolesQuery, LoadUsersAndRolesResponse>
+{
+    public async Task<LoadUsersAndRolesResponse> Handle(LoadUsersAndRolesQuery request, CancellationToken cancellationToken)
+    {
+        var roles = (await GetAppRoles.GetAppRoles()).Select(
+            p =>
+                new LoadUsersAndRolesResponse.LoadedRole(
+                    p.Id.GetValueOrDefault(),
+                    p.DisplayName ?? String.Empty
+                )
+        );
+
+        var roleAssignments = await GetAppRoleAssigment.GetAppRoleAssignments(cancellationToken);
+
+        var users = roleAssignments
+            .GroupBy(a => a.PrincipalId)
+            .Select(
+                group =>
+                    new LoadUsersAndRolesResponse.LoadedUser(
+                        group.First().PrincipalDisplayName ?? String.Empty,
+                        group.Key!.Value,
+                        group.Where(a => a.AppRoleId != Guid.Empty).Select(a => a.AppRoleId!.Value)
+                    )
+            );
+
+        return new LoadUsersAndRolesResponse(roles, users);
+    }
+}
